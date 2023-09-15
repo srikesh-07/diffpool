38d1407d2b0ecd055e285c55d6853a2b4f4b6f53
Srikesh <srikeshhp@gmail.com>
2023-09-15 14:30:43 +0530

    Bash update

--- train.sh
+++ train.sh
@@ -1,5 +1,7 @@
-python train.py --dataset DD --pool_ratio 0.10 --num_pool 2 --epochs 1000
-python train.py --dataset PTC_MR --pool_ratio 0.10 --num_pool 2 --epochs 1000
-python train.py --dataset PROTEINS --pool_ratio 0.10 --num_pool 2 --epochs 1000
-python train.py --dataset IMDB-BINARY --pool_ratio 0.10 --num_pool 2 --epochs 1000
-python train.py --dataset FRANKENSTEIN --pool_ratio 0.10 --num_pool 2 --epochs 1000
+CUDA_DEVICE=1
+
+CUDA_VISIBLE_DEVICES=$CUDA_DEVICE python train.py --dataset DD --pool_ratio 0.10 --num_pool 2 --epochs 1000
+CUDA_VISIBLE_DEVICES=$CUDA_DEVICE python train.py --dataset PTC_MR --pool_ratio 0.10 --num_pool 2 --epochs 1000
+CUDA_VISIBLE_DEVICES=$CUDA_DEVICE python train.py --dataset PROTEINS --pool_ratio 0.10 --num_pool 2 --epochs 1000
+CUDA_VISIBLE_DEVICES=$CUDA_DEVICE python train.py --dataset IMDB-BINARY --pool_ratio 0.10 --num_pool 2 --epochs 1000
+CUDA_VISIBLE_DEVICES=$CUDA_DEVICE python train.py --dataset FRANKENSTEIN --pool_ratio 0.10 --num_pool 2 --epochs 1000
